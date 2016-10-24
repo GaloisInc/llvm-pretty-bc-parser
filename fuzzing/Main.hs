a3da6ba0aa6b34663cf6787c7ead88b5cf31714b
Adam C. Foltzer <acfoltzer@galois.com>
2016-10-24 10:08:53 -0700

    Many improvements to the fuzzer:
    - Make runner more configurable
      - Can specify output directory for test cases and reductions
      - Can specify multiple `clang` executables to test a range of LLVM versions
    - Try to reduce noise by collapsing test failures by stderr contents
    - Automatically reduce test failures with Creduce by using `llvm-disasm`
      stack trace as the "interestingness" test
    - Parallelize Csmith step on the Haskell side (Creduce already takes
      advantage of multiple cores)
    - Handle clang errors gracefully (turns out a fuzzer is good at finding
      compiler bugs!)
    
    Squashed commit of the following:
    
    commit b2ec16f2fe92ebca02ba6eddda83dce1c4226402
    Author: Adam C. Foltzer <acfoltzer@galois.com>
    Date:   Mon Oct 24 09:10:32 2016 -0700
    
        parallelize Csmith step
    
    commit 4db416752719a8f3d0c18b1317b4defe1f4c323a
    Author: Adam C. Foltzer <acfoltzer@gmail.com>
    Date:   Thu Oct 20 21:32:59 2016 -0700
    
        handle clang errors
    
    commit 070b3ea421ae9ad54b7807f4f28c19bfd8abba04
    Author: Adam C. Foltzer <acfoltzer@galois.com>
    Date:   Thu Oct 20 18:47:20 2016 -0700
    
        unhide the Creduce output
    
    commit c86df9235b0b8b221955487b25f75b37ac67960b
    Author: Adam C. Foltzer <acfoltzer@galois.com>
    Date:   Thu Oct 20 18:42:03 2016 -0700
    
        remove absolute path stuff
    
    commit 1c7dcadb0807ade32f495c8eb95a13874b465370
    Author: Adam C. Foltzer <acfoltzer@galois.com>
    Date:   Thu Oct 20 18:36:50 2016 -0700
    
        add seed arg and Creduce driver
    
    commit 9d415491895f67102cca5520207becc2ea4026b8
    Author: Adam C. Foltzer <acfoltzer@galois.com>
    Date:   Thu Oct 20 16:44:32 2016 -0700
    
        tweak output directory creation
    
    commit 0f3a8e25bfbd609898132129969bb98ecb5c1ef4
    Author: Adam C. Foltzer <acfoltzer@galois.com>
    Date:   Thu Oct 20 15:58:26 2016 -0700
    
        implement test case saving and collapsing
    
    commit 8bce64d1d522de80db436086b2f8f7346e52bc98
    Author: Adam C. Foltzer <acfoltzer@galois.com>
    Date:   Thu Oct 20 15:26:41 2016 -0700
    
        fixup JUnit formatting and quiet success output
    
    commit 5028e9149625debf72b08938ca724f2515ad3cd5
    Author: Adam C. Foltzer <acfoltzer@galois.com>
    Date:   Thu Oct 20 14:45:29 2016 -0700
    
        add multiple clang versions for fuzzing runs
    
    commit eb5b08eff3dd69ce0b924f4a47ec8a23396dd3f8
    Author: Adam C. Foltzer <acfoltzer@galois.com>
    Date:   Wed Oct 19 17:49:01 2016 -0700
    
        hook up JUnit command line arg

--- fuzzing/Main.hs
+++ fuzzing/Main.hs
@@ -1,55 +1,95 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE RecordWildCards #-}
 module Main where
 
-import Control.Monad-- (forM_, when)
-import Data.List (partition)
---import qualified Data.Map.Strict as Map
+import Control.DeepSeq (($!!), NFData)
+import Control.Monad (forM, forM_, void, when)
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Par.Class (get, spawn)
+import Control.Monad.Par.IO (runParIO)
+import Data.List (isPrefixOf, partition)
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import Data.Maybe (fromMaybe)
 import Data.Monoid (mconcat, Endo(..))
 import Data.Time
+  (defaultTimeLocale, formatTime, getZonedTime, iso8601DateFormat)
 import Data.Word (Word64)
+import GHC.Generics (Generic)
 import System.Console.GetOpt
-           (ArgOrder(..), ArgDescr(..), OptDescr(..), getOpt, usageInfo)
+  (ArgOrder(..), ArgDescr(..), OptDescr(..), getOpt, usageInfo)
+import System.Directory
+  (copyFile, createDirectoryIfMissing, getFileSize, getPermissions,
+   setPermissions, setOwnerExecutable)
 import System.Environment (getArgs, getProgName, lookupEnv)
 import System.Exit (ExitCode(..), exitFailure, exitSuccess)
-import System.FilePath ((</>), (<.>))
+import System.FilePath ((</>), (<.>), dropExtension)
 import System.IO.Temp (withTempDirectory)
 import System.Process
+  (callProcess, readProcess, readProcessWithExitCode,
+   spawnProcess, waitForProcess)
 import System.Random (randomIO)
 import Text.Read (readMaybe)
-import Text.XML.Light
+import Text.XML.Light (Attr(..), Element, ppTopElement, unode, unqual)
 
 -- Option Parsing --------------------------------------------------------------
+
+-- | The name of the @clang@ executable for a particular test
+-- configuration, e.g., @clang-3.8@
+type Clang = String
 
 data Options = Options {
     optNumTests :: Integer
     -- ^ Number of Tests
+  , optSeeds :: Maybe [Word64]
+    -- ^ Specific seeds to use in fuzzing; overrides 'optNumTests'
   , optSaveTests :: Maybe FilePath
     -- ^ Location to save failed tests
+  , optClangs :: [Clang]
+    -- ^ Clangs to use with the fuzzer
   , optJUnitXml :: Maybe FilePath
     -- ^ Write JUnit test report
   , optCsmithPath :: Maybe FilePath
     -- ^ Path to Csmith include files
-  , optHelp    :: Bool
+  , optCollapse :: Bool
+    -- ^ Whether to collapse failures with the same error message
+  , optReduce :: Bool
+    -- ^ Whether to try reducing the test case with Creduce
+  , optHelp :: Bool
   } deriving (Show)
 
 defaultOptions :: Options
 defaultOptions  = Options {
     optNumTests   = 100
+  , optSeeds      = Nothing
   , optSaveTests  = Nothing
+  , optClangs     = ["clang"]
   , optJUnitXml   = Nothing
   , optCsmithPath = Nothing
+  , optCollapse   = False
+  , optReduce     = False
   , optHelp       = False
   }
 
 options :: [OptDescr (Endo Options)]
 options  =
   [ Option "n" [] (ReqArg setNumTests "NUMBER")
-    "number of tests to run"
-  , Option ""  ["save"] (ReqArg setSaveTests "DIRECTORY")
+    "number of tests to run; default is 100"
+  , Option "s" ["seed"] (ReqArg addSeed "SEED")
+    "specific Csmith seed to use; overrides -n"
+  , Option "o" ["output"] (ReqArg setSaveTests "DIRECTORY")
     "directory to save failed tests"
+  , Option "c" ["clang"] (ReqArg addClang "CLANG")
+    "specify clang executables to use, e.g., `-c clang-3.8 -c clang-3.9'"
   , Option ""  ["junit-xml"] (ReqArg setJUnitXml "FILEPATH")
     "output JUnit-style XML test report"
   , Option ""  ["csmith-path"] (ReqArg setCsmithPath "DIRECTORY")
     "path to Csmith include files; default is $CSMITH_PATH environment variable"
+  , Option ""  ["collapse"] (NoArg setCollapse)
+    "collapse failing test cases by error message and remove successes"
+  , Option ""  ["reduce"] (NoArg setReduce)
+    "reduce failing test cases with a best-guess Creduce (requires `--output`)"
   , Option "h" ["help"] (NoArg setHelp)
     "display this message"
   ]
@@ -57,17 +97,38 @@ options  =
 setNumTests :: String -> Endo Options
 setNumTests str = Endo $ \opt ->
   case readMaybe str of
-    Just n -> opt { optNumTests = n }
     Nothing -> error "expected integer number of tests"
+    Just n -> opt { optNumTests = n }
+
+addSeed :: String -> Endo Options
+addSeed str = Endo $ \opt ->
+  case readMaybe str of
+    Nothing -> error "expected 64-bit integer seed"
+    Just n ->
+      case optSeeds opt of
+        Nothing    -> opt { optSeeds = Just [n] }
+        Just seeds -> opt { optSeeds = Just (n : seeds) }
 
 setSaveTests :: String -> Endo Options
 setSaveTests str = Endo (\opt -> opt { optSaveTests = Just str })
+
+addClang :: String -> Endo Options
+addClang str = Endo $ \opt ->
+  case optClangs opt of
+    ["clang"] -> opt { optClangs = [str] }
+    clangs    -> opt { optClangs = str:clangs }
 
 setJUnitXml :: String -> Endo Options
 setJUnitXml str = Endo (\opt -> opt { optJUnitXml = Just str })
 
 setCsmithPath :: String -> Endo Options
 setCsmithPath str = Endo (\opt -> opt { optCsmithPath = Just str })
+
+setCollapse :: Endo Options
+setCollapse = Endo (\opt -> opt { optCollapse = True })
+
+setReduce :: Endo Options
+setReduce = Endo (\opt -> opt { optReduce = True })
 
 setHelp :: Endo Options
 setHelp = Endo (\opt -> opt { optHelp = True })
@@ -94,29 +155,139 @@ printUsage errs =
      putStrLn (usageInfo (unlines (errs ++ [banner])) options)
 
 main :: IO ()
-main = do
+main = withTempDirectory "." ".fuzz." $ \tmpDir -> do
   opts <- getOptions
-  hostname <- readProcess "hostname" [] ""
-  now <- getZonedTime
-  results <- forM [1..optNumTests opts] $ \_ -> do
-    seed <- randomIO
-    runTest seed opts
-  let (_passes, fails) = partition isPass results
-  when (not (null fails)) $ do
-    putStrLn $
-      show (length fails) ++ "/" ++ show (optNumTests opts) ++ " tests failed."
-    putStrLn "Failed test seeds:"
-    forM_ fails $ \(TestFail s _ _) ->
-      print s
-    putStr (ppTopElement (mkJUnitXml hostname now results))
-    exitFailure
+  when (optSaveTests opts == Nothing && optReduce opts) $
+    printUsage [ "--reduce requires --output to be set" ]
+  -- run the tests within each clang version in parallel. We could
+  -- parallelize the runs across clang versions as well, but it's
+  -- probably not worth the complexity at that level of granularity
+  resultMaps <-
+    forM (optClangs opts) $ \clang -> runParIO $ do
+      liftIO $ putStrLn $ "[" ++ clang ++ "]"
+      results' <-
+        case optSeeds opts of
+          Nothing ->
+            forM [1..optNumTests opts] $ \_ ->
+              spawn $ liftIO $ do
+              seed <- randomIO
+              runTest tmpDir clang seed opts
+          Just seeds ->
+            forM seeds $ \seed ->
+              spawn $ liftIO $ do
+              runTest tmpDir clang seed opts
+      results <- mapM get results'
+      return (Map.singleton clang results)
+  let allResults' = Map.unions resultMaps
+      allResults | optCollapse opts = collapseResults allResults'
+                 | otherwise        = allResults'
+  forM_ (Map.toList allResults) $ \(clang, results) -> do
+    let (_passes, fails) = partition isPass results
+    when (not (null fails)) $ do
+      putStrLn $ "[" ++ clang ++ "] " ++
+        show (length fails) ++ " failing cases identified:"
+      forM_ fails $ \(TestFail s _ _) ->
+        print s
+  case optSaveTests opts of
+    Nothing -> return ()
+    Just root -> do
+      createDirectoryIfMissing False root
+      forM_ (Map.toList allResults) $ \(clang, results) ->
+        when (not (null (filter isFail results))) $ do
+          let clangRoot = root </> clang
+          createDirectoryIfMissing False clangRoot
+          forM_ results $ \result ->
+            case result of
+              TestPass _ -> return ()
+              -- errors arise from bugs in clang, not our code
+              TestError _ -> return ()
+              TestFail _ TestSrc{..} err -> do
+                copyFile (tmpDir </> srcFile) (clangRoot </> srcFile)
+                writeFile (clangRoot </> srcFile <.> "stderr") err
+                when (optReduce opts) $
+                  reduce clang opts clangRoot srcFile err
+  case optJUnitXml opts of
+    Nothing -> return ()
+    Just f -> do
+      xml <- mkJUnitXml allResults
+      writeFile f (ppTopElement xml)
+
+-- | Best-effort reduction of test cases using Creduce. Much of the
+-- art of using Creduce is in writing a correct interestingness test,
+-- which we try to approximate here using the stack trace from
+-- @llvm-disasm@. We also do not introduce any parallelism here
+-- because Creduce introduces its own parallelism when we invoke it.
+reduce :: Clang -> Options -> FilePath -> FilePath -> String -> IO ()
+reduce clang opts clangRoot srcFile err = do
+  csmithPath <- getCsmithPath opts
+  -- copy a file for the reduction in place
+  let baseName   = dropExtension srcFile
+      srcReduced = clangRoot </> baseName ++ "-reduced.c"
+      scriptFile = clangRoot </> baseName ++ "-reduce.sh"
+  copyFile (clangRoot </> srcFile) srcReduced
+  -- find the best guess at the grep pattern for the Creduce script by
+  -- looking for the first line starting with a tab; this should be
+  -- the top of the stack trace
+  let grepPat =
+        case dropWhile (not . isPrefixOf "\t") (lines err) of
+          -- if we don't have a stack trace, we probably shouldn't
+          -- waste our time
+          [] -> Nothing
+          first:_ -> Just first
+      script = unlines [
+          "#!/usr/bin/env bash"
+        , "# Consider this script a best guess template for reducing failing"
+        , "# test cases. Not every bug will manifest in the same way and give"
+        , "# the same error message, so modify the grep condition appropriately"
+        , "# if the shrink is unsatisfactory."
+        , ""
+        , concat [ clang, " -I", csmithPath, " -O -g -w -c "
+                 , "-emit-llvm ", baseName ++"-reduced.c", " -o ", baseName <.> "bc" ]
+        , "if [ $? -ne 0 ]; then"
+        , "    exit 1"
+        , "fi"
+        , concat [ "llvm-disasm ", baseName <.> "bc", " 2>&1 | "
+                 , "grep '^", fromMaybe "" grepPat, "'"]
+        ]
+  when (grepPat /= Nothing) $ do
+    -- write out the shell script to drive Creduce and run it
+    writeFile scriptFile script
+    p <- getPermissions scriptFile
+    setPermissions scriptFile (setOwnerExecutable True p)
+    h <- spawnProcess "creduce" [ scriptFile, srcReduced ]
+    void $ waitForProcess h
+
+collapseResults :: Map Clang [TestResult] -> Map Clang [TestResult]
+collapseResults = Map.map (collapse Map.empty)
+  where
+    collapse seen [] = Map.elems seen
+    collapse seen (r:results) =
+      case r of
+        TestPass _ -> collapse seen results
+        -- errors arise from bugs in clang, not our code
+        TestError _ -> collapse seen results
+        TestFail _ _ err ->
+          collapse (Map.insertWith f err r seen) results
+    -- choose the smallest source file
+    f r1@(TestFail _ src1 _) r2@(TestFail _ src2 _) =
+      case compare (srcSize src1) (srcSize src2) of
+        LT -> r1
+        EQ -> r1 -- arbitrarily
+        GT -> r2
+    f _ _ = error "only TestFails should go in this map"
 
 type Seed = Word64
 
 data TestResult
   = TestPass Seed
-  | TestFail Seed FilePath String
-  deriving (Eq, Show)
+  | TestFail Seed TestSrc String
+  | TestError Seed
+  -- ^ For now, errors are treated the same as passes, since we're not
+  -- concerned with clang bugs
+  deriving (Eq, Show, Generic, NFData)
+
+data TestSrc = TestSrc { srcFile :: FilePath, srcSize :: Integer }
+  deriving (Eq, Show, Generic, NFData)
 
 isPass :: TestResult -> Bool
 isPass (TestPass _) = True
@@ -125,31 +296,41 @@ isPass _            = False
 isFail :: TestResult -> Bool
 isFail = not . isPass
 
-runTest :: Seed -> Options -> IO TestResult
-runTest seed opts = withTempDirectory "." ".fuzz." $ \tmpDir -> do
-  let testSrc = tmpDir </> "test-" ++ show seed <.> "c"
+runTest :: FilePath -> Clang -> Seed -> Options -> IO TestResult
+runTest tmpDir clang seed opts = do
+  let baseFile = clang ++ "-" ++ show seed
+      srcFile  = baseFile <.> "c"
+      bcFile   = baseFile <.> "bc"
   csmithPath <- getCsmithPath opts
   callProcess "csmith" [
-      "-o", tmpDir </> "test.c"
+      "-o", tmpDir </> srcFile
     , "-s", show seed
     ]
-  callProcess "clang" [
+  h <- spawnProcess clang [
       "-I" ++ csmithPath
     , "-O", "-g", "-w", "-c", "-emit-llvm"
-    , tmpDir </> "test.c"
-    , "-o", tmpDir </> "test.bc"
+    , tmpDir </> srcFile
+    , "-o", tmpDir </> bcFile
     ]
-  (ec, out, err) <-
-    readProcessWithExitCode "llvm-disasm" [ tmpDir </> "test.bc" ] ""
-  putStrLn "[OUT]"
-  putStr out
-  putStrLn "[ERR]"
-  putStr err
-  case ec of
-    ExitSuccess -> return (TestPass seed)
-    ExitFailure c -> do
-      putStrLn ("[ERROR CODE " ++ show c ++ "]")
-      return (TestFail seed testSrc err)
+  clangErr <- waitForProcess h
+  case clangErr of
+    ExitFailure _ -> return (TestError seed)
+    _ -> do
+      (ec, out, err) <-
+        readProcessWithExitCode "llvm-disasm" [ tmpDir </> bcFile ] ""
+      case ec of
+        ExitSuccess -> do
+          putStrLn "[PASS]"
+          return (TestPass seed)
+        ExitFailure c -> do
+          putStrLn "[ERROR]"
+          putStrLn "[OUT]"
+          putStr out
+          putStrLn "[ERR]"
+          putStr err
+          putStrLn ("[ERROR CODE " ++ show c ++ "]")
+          srcSize <- getFileSize (tmpDir </> srcFile)
+          return $!! TestFail seed TestSrc{..} err
 
 getCsmithPath :: Options -> IO FilePath
 getCsmithPath opts =
@@ -161,47 +342,61 @@ getCsmithPath opts =
         Just p -> return p
         Nothing -> error "--csmith-path not given and CSMITH_PATH not set"
 
-mkJUnitXml :: String -> ZonedTime -> [TestResult] -> Element
-mkJUnitXml hostname now results =
-  unode "testsuites" $
-  unode "testsuite" ([
-      uattr "name"      "llvm-disasm fuzzer"
-    , uattr "tests"     (show (length results))
-    , uattr "failures"  (show (length fails))
-    , uattr "errors"    "0"
-    , uattr "skipped"   "0"
-    , uattr "timestamp" nowFmt
-    , uattr "time"      "0.0" -- irrelevant due to random input
-    , uattr "id"        ""
-    , uattr "package"   ""
-    , uattr "hostname"  hostname
-    ]
-    , flip map results $ \res ->
-        case res of
-          TestPass seed ->
-            unode "testcase" [
-                uattr "name"      (show seed)
-              , uattr "classname" ""
-              , uattr "time"      "0.0"
-              ]
-          TestFail seed _ err ->
-            unode "testcase" ([
-                uattr "name"      (show seed)
-              , uattr "classname" ""
-              , uattr "time"      "0.0"
-              ]
-              , unode "failure" ([
-                    uattr "message" ""
-                  , uattr "type"    ""
-                  ]
-                  , err
-                  )
-              )
-    )
+mkJUnitXml :: Map Clang [TestResult] -> IO Element
+mkJUnitXml allResults = do
+  hostname <- readProcess "hostname" [] ""
+  now <- getZonedTime
+  let nowFmt = formatTime
+                 defaultTimeLocale
+                 (iso8601DateFormat (Just "%H:%M:%S"))
+                 now
+      testsuites = map (testsuite hostname nowFmt) (Map.toList allResults)
+  return $ unode "testsuites" testsuites
   where
-  (_passes, fails) = partition isPass results
-  nowFmt =
-    formatTime defaultTimeLocale (iso8601DateFormat (Just "%H:%M:%S")) now
+    toUnder = map (\c -> if c == '.' then '_' else c)
+    testsuite hostname nowFmt (clang, results) =
+      unode "testsuite" ([
+          uattr "name"      "llvm-disasm fuzzer"
+        , uattr "tests"     (show (length results))
+        , uattr "failures"  (show (length (filter isFail results)))
+        , uattr "errors"    "0"
+        , uattr "skipped"   "0"
+        , uattr "timestamp" nowFmt
+        , uattr "time"      "0.0" -- irrelevant due to random input
+        , uattr "id"        ""
+        , uattr "package"   (toUnder clang)
+        , uattr "hostname"  hostname
+        ]
+        , flip map results $ \res ->
+            case res of
+              TestPass seed ->
+                unode "testcase" [
+                    uattr "name"      (show seed)
+                  , uattr "classname" (toUnder clang)
+                  , uattr "time"      "0.0"
+                  ]
+              TestPass seed ->
+                unode "testcase" ([
+                    uattr "name"      (show seed)
+                  , uattr "classname" (toUnder clang)
+                  , uattr "time"      "0.0"
+                  ]
+                  , "clang error"
+                  )
+              TestFail seed _ err ->
+                unode "testcase" ([
+                    uattr "name"      (show seed)
+                  , uattr "classname" (toUnder clang)
+                  , uattr "time"      "0.0"
+                  ]
+                  , unode "failure" ([
+                        uattr "message" ""
+                      , uattr "type"    ""
+                      ]
+                      , err
+                      )
+                  )
+        )
 
 uattr :: String -> String -> Attr
 uattr k v = Attr (unqual k) v
