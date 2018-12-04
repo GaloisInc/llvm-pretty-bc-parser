66cd05bf1a0a9aef57cf3da7ba8e5a13aa44c375
Langston Barrett <langston.barrett@gmail.com>
2018-12-04 13:54:41 -0800

    Include forgotten 'Internal' module

---
+++ src/Data/LLVM/Internal.hs
@@ -0,0 +1,6 @@
+-- | This module exports some internal modules *for use in testing only*.
+module Data.LLVM.Internal
+  ( module Data.LLVM.BitCode.IR.Metadata
+  ) where
+
+import Data.LLVM.BitCode.IR.Metadata
