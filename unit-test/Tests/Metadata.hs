71ba4dd35b5ba340bf7a17be63bb6acdd85b4f26
Valentin Robert <val@galois.com>
2021-10-28 10:10:42 -0700

    fix unit tests

--- unit-test/Tests/Metadata.hs
+++ unit-test/Tests/Metadata.hs
@@ -1,9 +1,8 @@
 module Tests.Metadata (tests) where
 
+import qualified Data.Sequence as Seq
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.HUnit (testCase, (@?=))
-import Test.Tasty.QuickCheck (testProperty)
-import Test.QuickCheck ((==>))
 
 import Data.LLVM.Internal
 import Text.LLVM.AST
@@ -11,9 +10,9 @@ import Text.LLVM.AST
 import Tests.Instances()
 
 tests :: TestTree
-tests = testGroup "Metadata" $
+tests = testGroup "Metadata"
     [ testCase "test dedup metadata" $
-        [] @?= dedupMetadata []
+        mempty @?= dedupMetadata mempty
 
     -- The first two should remain unchanged, but the third duplicates information in
     -- the second, so should be updated to hold a reference to it.
@@ -22,13 +21,14 @@ tests = testGroup "Metadata" $
             val1 = mkPum 1 (ValMdString "str")
             val2 = mkPum 2 (ValMdDebugInfo (DebugInfoExpression (DIExpression [])))
             val3 = mkPum 3 (ValMdNode [Just (pumValues val2)])
+            deduped = dedupMetadata (Seq.fromList [val1, val2, val3])
         in [ testCase "1" $
-               val1 @?= dedupMetadata [val1, val2, val3] !! 0
+               Just val1 @?= deduped Seq.!? 0
            , testCase "2" $
-               val2 @?= dedupMetadata [val1, val2, val3] !! 1
+               Just val2 @?= deduped Seq.!? 1
            , testCase "3" $
-               mkPum 3 (ValMdNode [Just (ValMdRef 2)]) @?=
-                  dedupMetadata [val1, val2, val3] !! 2
+               Just (mkPum 3 (ValMdNode [Just (ValMdRef 2)])) @?=
+                  deduped Seq.!? 2
            ]
 
     -- Deduplication should not changes: references or strings
