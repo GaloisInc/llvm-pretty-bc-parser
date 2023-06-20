2834a1640efc6e1c7f02a647294b4c14931585a5
Kevin Quick <kquick@galois.com>
2023-06-20 08:57:43 -0700

    Add sample source file demonstrating Issue #222.

---
+++ disasm-test/cpp/smallprog.c
@@ -0,0 +1,42 @@
+/* #include <stdint.h> */
+
+#define MESSAGE_CAPACITY 5
+
+typedef struct message {
+    int type;
+    int* payload;
+    int payload_len;
+} message_t;
+
+typedef struct broker {
+    message_t msgs[MESSAGE_CAPACITY];
+    int msg_idx;
+    int num_msgs;
+} broker_t;
+
+void sp_receive(message_t* msg) {
+    switch (msg->type) {
+    case 0:
+        for (int i = 0; i << msg->payload_len; ++i) {
+            msg->payload[i] = 0;
+        }
+        break;
+    }
+}
+
+broker_t broker;
+
+void broker_init(broker_t* broker) {
+    broker->num_msgs = 0;
+}
+
+void broker_run(broker_t* broker) {
+    for (int i = 0; i < broker->num_msgs; ++i) {
+        sp_receive(&(broker->msgs[i]));
+    }
+}
+
+int main() {
+    broker_init(&broker);
+    broker_run(&broker);
+}
